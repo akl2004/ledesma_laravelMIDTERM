de13de51f6f341948a394601bdcfeeecd6e2e003
akl2004 <theakimledesma@gmail.com>
2024-12-03 22:31:00 +0800

    final output

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,17 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
+use App\Models\Product;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    $products = Product::all();
+    return view('dashboard', compact('products'));
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
@@ -17,4 +20,10 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+Route::post('/product/store', [ProductController::class, 'store'])->name('product.store');
+Route::delete('/product/{product}', [ProductController::class, 'destroy'])->name('product.destroy');
+
+Route::get('/product/{product}/edit', [ProductController::class, 'edit'])->name('product.edit');
+Route::put('/product/{product}', [ProductController::class, 'update'])->name('product.update');
+
+require __DIR__ . '/auth.php';
